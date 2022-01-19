a4841e3104cefad6e96bdf2f9d63b9ca4114a802
jonytalukdar <jonytalukdar30@gmail.com>
2022-01-19 06:50:01 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
+import { Layout, Typography, Space } from 'antd';
+import { Switch, Route, Link } from 'react-router-dom';
+
+import './App.css';
+
+import { Navbar } from './components';
+
 const App = () => {
   return (
-    <div>
-      <h1>Helllo crypto</h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main"></div>
+      <div className="footer"></div>
     </div>
   );
 };
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { Button, Typography, Menu, Avatar } from 'antd';
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from '@ant-design/icons';
+
+import icon from '../images/cryptocurrency.png';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          Crypto Currency
+        </Typography.Title>
+      </div>
+      <Menu theme="dark">
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/crypto">Cryptocurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchange">Exchange</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
+      </Menu>
+    </div>
+  );
+};
+
+export default Navbar;
